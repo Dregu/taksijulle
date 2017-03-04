c0fc661b3e2aca6ce3566ab0588447bec954bc0d
Dregu <dregu@dreg.us>
2017-03-04 15:51:35 +0200

    paska api

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var 	nconf = require('nconf')
 nconf.argv().env().file({ file: 'config.json' })
 var	TelegramBot = require('node-telegram-bot-api'),
 	moment = require('moment-timezone'),
-	GC = require('public-google-calendar'),
-	gc = new GC({
-		calendarId: nconf.get('calid')
-	}),
 	token = nconf.get('token'),
-	bot = new TelegramBot(token, { polling: true })
+	bot = new TelegramBot(token, { polling: true }),
+	request = require('request'),
+	apikey = nconf.get('apikey'),
+	calid = nconf.get('calid')
 
 bot.onText(/\/taksi ?(.*)/, (msg, match) => {
-	gc.getEvents({ earliestFirst: true }, (err, events) => {
-		if (err) {
-			return console.log(err.message)
+	request.get('https://www.googleapis.com/calendar/v3/calendars/'+calid+'/events?key='+apikey+'&singleEvents=true&orderBy=startTime', function (err, res, body) {
+		if (err || res.statusCode != 200) {
+			return console.log(err)
 		}
+		var events = JSON.parse(body)
 		var now = moment()
 		var event
-		events.some((e) => {
-			e.start = moment(e.start)
-			e.end = moment(e.end)
+		events.items.some((e) => {
+			e.start = moment(e.start.dateTime)
+			e.end = moment(e.end.dateTime)
 			if((e.start < now && e.end > now) || e.start > now) {
 				e.now = (e.start < now)
 				return event = e
@@ -31,10 +31,10 @@ bot.onText(/\/taksi ?(.*)/, (msg, match) => {
 		}
 		if (event.now) {
 			var shift = event.start.tz('Europe/Helsinki').format('H:mm')+'—'+event.end.tz('Europe/Helsinki').format('H:mm')
-			bot.sendMessage(msg.chat.id, '👍 TaksiJulle *on vuorossa* ('+shift+'), soita heti 😉 045 7875 6969 🍆', { parse_mode: 'Markdown' })
+			bot.sendMessage(msg.chat.id, '👍 @TaksiJulle *on vuorossa* ('+shift+'), soita heti 😉 045 7875 6969 🍆', { parse_mode: 'Markdown' })
 		} else {
 			var shift = event.start.tz('Europe/Helsinki').format('D.M. H:mm')+'—'+event.end.tz('Europe/Helsinki').format('H:mm')
-			bot.sendMessage(msg.chat.id, '👎 TaksiJulle *ei ole vuorossa* (seuraava vuoro '+shift+')', { parse_mode: 'Markdown' })
+			bot.sendMessage(msg.chat.id, '👎 @TaksiJulle *ei ole vuorossa* (seuraava vuoro '+shift+')', { parse_mode: 'Markdown' })
 		}
 	})
 })
@@ -43,6 +43,17 @@ bot.onText(/\/numero(.*)/, (msg, match) => {
 	bot.sendContact(msg.chat.id, '+3584578756969', 'TaksiJulle')
 })
 
+bot.onText(/\/start/, (msg, match) => {
+	if (msg.chat.type == 'private') {
+		bot.sendMessage(msg.chat.id, 'Olen @TaksiJulle:n botti. Kerron onko /taksi ajossa.')
+		bot.sendContact(msg.chat.id, '+3584578756969', 'TaksiJulle')
+	}
+})
+
 bot.onText(/(.*)/, (msg, match) => {
 	console.log(msg)
+	if (msg.text.match(/^[^\/].*/) && msg.chat.type == 'private') {
+		bot.sendMessage(msg.chat.id, 'Älä minulle yritä jutella, olen botti. @TaksiJulle vastaa viesteihisi tai numerosta:')
+		bot.sendContact(msg.chat.id, '+3584578756969', 'TaksiJulle')
+	}
 })
